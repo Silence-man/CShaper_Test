b91febb99188528a26ce29d1a6d26a70095214e3
Silence-man <364721321@qq.com>
2017-12-06 15:39:34 +0800

    C#数组属性索引器

---
+++ Array_Get_Set/Array_Get_Set/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Get_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            int[] arr = new int[5] { 1, 2, 3, 4, 5 };
+            //t.Ids = arr;//会调用属性的Set方法
+            t.Ids[1] = 1;//正常访问数组  调用属性的Get方法  返回对象给t.Ids   
+            //跟下标无关 下标赋值是数组自己的功能
+            t[2]=6;//索引器访问数组
+
+            for (int i = 0; i < t.Ids.Length; i++)
+            {
+                Console.WriteLine(t.Ids[i]);
+            }
+            Console.Read();
+        }
+    }
+    
+    class Test
+    {
+        private int[] ids=new int[5];
+
+        //属性封装的是整个数据对象 不是具体的单独类型
+        public int[] Ids
+        {
+            get
+            {
+                return ids;
+            }
+
+            //set
+            //{
+            //    ids = value;
+            //}
+        }
+
+        public int this[int nindex]
+        {
+            get { return ids[nindex]; }
+            set { ids[nindex] = value; }
+        }
+    }
+}
---
+++ Array_Get_Set/Indexer/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Jeff Wong";
+            Console.WriteLine(student.Name);
+            Console.Read();
+
+            //Student student = new Student();
+            //int num = student.ListStudents.Count;
+            //Console.WriteLine("All the students:\nLeft-Attributes Right-Indexer\n");
+            //for (int i = 0; i < num; i++)
+            //{
+            //    Console.Write(student.ListStudents[i].Name); //通过属性，取所有学生名
+            //    Console.Write("-----------"); 
+            //    Console.WriteLine(student[i].Name); //通过索引器，取所有学生名
+            //}
+        }
+    }
+    public class Student
+    {
+        private string name;
+        /// <summary>
+        /// 定义学生的姓名属性
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Student()
+        { }
+        public Student(string str)
+        {
+            this.Name = str;
+        }
+        //public Student()
+        //{
+        //    this.listStudents.Add(new Student("jeff wong"));
+        //    this.listStudents.Add(new Student("jeffery zhao"));
+        //    this.listStudents.Add(new Student("terry lee"));
+        //    this.listStudents.Add(new Student("dudu"));
+        //}
+        private List<Student> listStudents = new List<Student>();
+        public List<Student> ListStudents
+        {
+            get
+            {
+                return listStudents;
+            }
+
+            set
+            {
+                listStudents = value;
+            }
+        }
+
+
+        /// <summary>
+        /// 构建索引器
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public Student this[int i]
+        {
+            get { return listStudents[i]; }
+            set { listStudents[i] = value; }
+        }
+
+    }
+}
